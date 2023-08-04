e67f1ffb8ad826f874d0dab1e8da685597601db5
Lawrence Naumov <prostolawr@gmail.com>
2023-08-04 20:20:28 +0800

    Change font to Open Sans

--- src/App.vue
+++ src/App.vue
@@ -4,14 +4,16 @@
 
 <style lang="scss">
 @import "src/assets/reset-styles";
+
 body {
   display: flex;
   flex-direction: column;
   align-items: center;
-  font-family: "Manrope", sans-serif;
+  font-family: "Open Sans", sans-serif;
   background-color: #bfe0f3;
   height: 100%;
 }
+
 .content {
   max-width: 500px;
 }
